7ce633cd17164d86169d0f11aad7045bd097ec9d
William Gu <william.gu@itrm.co.uk>
2022-09-27 08:42:08 +0100

    auth setup

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Route;
     use App\Http\Controllers\StudentController;
+    use App\Http\Controllers\Auth\LoginController;
 
     /*
     |--------------------------------------------------------------------------
@@ -18,6 +19,8 @@
     Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::post('/login', LoginController::class . '@login')->name('api-login');
 
     Route::group(['prefix' => 'students'], function () {
         Route::post('/search', StudentController::class . '@search')->name('students-search');
